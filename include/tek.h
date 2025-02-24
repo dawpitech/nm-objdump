1831c1133e054f316e24ac84e163934fd44c60ff
Antoine <me@antoinebellanger.fr>
2025-02-24 19:16:03 +0100

    fix: error messages and args handling

---
+++ include/tek.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2025
+** nm-objdump
+** File description:
+** tek.h
+*/
+
+#ifndef TEK_H
+    #define TEK_H
+
+    #define EXIT_FAILURE_TEK 84
+#endif //TEK_H
--- src/nm/nm.c
+++ src/nm/nm.c
@@ -11,10 +11,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+
+#include "tek.h"
 
 static char get_symbol_ex(const Elf64_Sym symbol, const Elf64_Shdr *shdr,
     const bool is_global)
@@ -67,35 +68,35 @@ static void print_symbol_info(const Elf64_Sym symbol, const Elf64_Shdr *shdr,
         sym_strtab + symbol.st_name);
 }
 
-static int print_error(const char *str)
+static void print_err(const char *file, const char *msg)
 {
-    (void) !write(1, str, strlen(str));
-    return 84;
-}
-
-static void *print_error_null(const char *str)
-{
-    (void) !write(1, str, strlen(str));
-    return NULL;
+    fprintf(stderr, "nm: %s: %s\n", file, msg);
 }
 
 static Elf64_Ehdr *load_elf(const char *filepath, char **buff, struct stat *s,
     int *fd)
 {
     *fd = open(filepath, O_RDONLY);
-    if (*fd == -1)
-        return print_error_null("Couldn't open file.\n");
+    if (*fd == -1) {
+        print_err(filepath, "cannot open file");
+        return NULL;
+    }
     fstat(*fd, s);
     *buff = mmap(NULL, s->st_size, PROT_READ, MAP_PRIVATE, *fd, 0);
-    if (*buff == MAP_FAILED)
-        return print_error_null("Couldn't load file content.\n");
+    if (*buff == MAP_FAILED) {
+        print_err(filepath, "map failed");
+        return NULL;
+    }
     if ((*buff)[0] != ELFMAG0 || (*buff)[1] != ELFMAG1 ||
-        (*buff)[2] != ELFMAG2 || (*buff)[3] != ELFMAG3)
-        return print_error_null("nm: file format not recognized.\n");
+        (*buff)[2] != ELFMAG2 || (*buff)[3] != ELFMAG3) {
+        print_err(filepath, "file format not recognized");
+        return NULL;
+    }
     return (void *) *buff;
 }
 
-void iterate_symbols(const Elf64_Ehdr *elf, const char *buff)
+static void iterate_symbols(const Elf64_Ehdr *elf, const char *buff,
+    const char *filepath)
 {
     const Elf64_Shdr *shdr = (Elf64_Shdr *) (buff + elf->e_shoff);
     const Elf64_Shdr *sym_table = NULL;
@@ -107,7 +108,7 @@ void iterate_symbols(const Elf64_Ehdr *elf, const char *buff)
         if (shdr[i].sh_type == SHT_SYMTAB)
             sym_table = &shdr[i];
     if (sym_table == NULL) {
-        print_error("Couldn't find symbol table in given file.\n");
+        print_err(filepath, "Couldn't find symbol table in given file.\n");
         return;
     }
     symtab = &shdr[sym_table->sh_link];
@@ -117,20 +118,31 @@ void iterate_symbols(const Elf64_Ehdr *elf, const char *buff)
         print_symbol_info(sym[i], shdr, sym_strtab);
 }
 
-int main(const int argc, const char **argv)
+static int iterate_on_files(const char *const *files, const int size)
 {
     int fd = -1;
     char *buff = NULL;
     struct stat s;
     const Elf64_Ehdr *elf = NULL;
 
-    if (argc != 2)
-        return print_error("Not enough argument given.\n");
-    elf = load_elf(argv[1], &buff, &s, &fd);
-    if (elf == NULL)
-        return print_error("Couldn't load elf header.\n");
-    iterate_symbols(elf, buff);
-    munmap(buff, s.st_size);
-    close(fd);
+    for (int i = 0; i < size; i++) {
+        if (size > 1)
+            printf("\n%s:\n", files[i]);
+        elf = load_elf(files[i], &buff, &s, &fd);
+        if (elf == NULL)
+            return EXIT_FAILURE_TEK;
+        iterate_symbols(elf, buff, files[i]);
+        munmap(buff, s.st_size);
+        close(fd);
+    }
     return 0;
+}
+
+int main(const int argc, const char **argv)
+{
+    const char *const DEFAULT = "a.out";
+
+    if (argc > 1)
+        return iterate_on_files(&argv[1], argc - 1);
+    return iterate_on_files(&DEFAULT, 1);
 }
